9c1315c0196715a6da384acd31ce285287deb61d
Uncle <russkienjn@gmail.com>
2024-08-15 00:05:05 +0300

    add webpack additional import

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
